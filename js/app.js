7212507877be560bebc57c9c7cb9e6330c78317e
Dmitriy Feoktistov <feoktistovdmitriy@gmail.com>
2018-03-01 21:41:49 +0700

    Set up brand new variables and engine

--- js/app.js
+++ js/app.js
@@ -1,6 +1,4 @@
-/*
- * Create a list that holds all of your cards
- */
+// array to store types of cards icons
 const cardImage = [
   'fa-diamond',
   'fa-diamond',
@@ -19,15 +17,41 @@ const cardImage = [
   'fa-bomb',
   'fa-bomb'
 ];
+
+// Selector for deck
 const deck = document.querySelector('.deck');
+
+// making array from all selected cards
 const cards = Array.from(document.querySelectorAll('.card'));
-let cardIcon;
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+
+// array to store opened cards
+let openCards = [];
+
+// initial variable to store moves
+let moves = 0;
+
+let count = document.querySelector('.moves');
+const starCount = document.querySelectorAll('.fa-star');
+
+// counts number of cards matched to determine win condition
+let matchList = 0;
+
+// variables for timer
+const timer = document.querySelector('.game-timer');
+let second = 0;
+let minute = 0;
+let timePassed;
+
+// to avoid clicking the same card two times
+let clicked = true;
+
+const finishRating = document.querySelector('.rating');
+const finishTime = document.querySelector('.end-time');
+const finishMoves = document.querySelector('.total-moves');
+const star = document.querySelector('.stars');
+const modal = document.querySelector('.modal');
+const replayButton = document.querySelector('.replay');
+const replayGame = document.querySelector('.restart');
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 shuffle(cardImage);
@@ -50,40 +74,3 @@ function shuffle(cardImage) {
 
   return cardImage;
 }
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-function flipCard(e) {
-  if (e.target.classList == 'card' || e.target.classList == 'card open') {
-    e.target.classList.toggle('open');
-  } else if (
-    e.target.classList == 'fa fa-diamond' ||
-    e.target.classList == 'fa fa-paper-plane-o' ||
-    e.target.classList == 'fa fa-anchor' ||
-    e.target.classList == 'fa fa-bolt' ||
-    e.target.classList == 'fa fa-cube' ||
-    e.target.classList == 'fa fa-leaf' ||
-    e.target.classList == 'fa fa-bicycle' ||
-    e.target.classList == 'fa fa-bomb'
-  ) {
-    e.target.parentElement.classList.toggle('open');
-  }
-  // console.log(ic.classList);
-}
-
-function addCardToCompare(e) {
-  if (e.target.classList == 'card' || e.target.classList == 'card open') {
-    cardIcon = e.target.firstElementChild.classList;
-  }
-  console.log(cardIcon);
-}
-deck.addEventListener('click', flipCard);
-deck.addEventListener('click', addCardToCompare);
